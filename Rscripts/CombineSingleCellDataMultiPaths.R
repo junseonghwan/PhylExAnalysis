8ef004e1e9216eb4ded2241b6bc4da1c2afc9120
Seong-Hwan Jun <s2jun.uw@gmail.com>
2021-06-04 17:35:08 -0700

    Allow user to pass in paths separated by comma.

--- Rscripts/CombineSingleCellDataMultiPaths.R
+++ Rscripts/CombineSingleCellDataMultiPaths.R
@@ -2,15 +2,18 @@
 args = commandArgs(trailingOnly=TRUE)
 print(args)
 
-sc_reads_path <- args[1]
+# sc_reads_paths is a string containing paths, separated by comma. 
+sc_reads_paths <- args[1]
 loci_path <- args[2]
 output_path <- args[3]
 
 library(PhylExR)
+library(stringr)
 
 loci <- read.table(loci_path, header = T)
 
-sc <- CombineSingleCellReadsMultiPaths(sc_reads_path, file_separator = "\t")
+sc_reads_paths_ <- str_split(sc_reads_paths, pattern = ",")[[1]]
+sc <- CombineSingleCellReadsMultiPaths(sc_reads_paths_, file_separator = "\t")
 sc_ <- sc[!(sc$a == 0 & sc$d == 0),]
 
 ret <- sc_ %>% group_by(Cell) %>% summarise(n = sum(d - a > 0))
